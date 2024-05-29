24376025dd53641a0debb3035d3348105372f339
Cong Quy PC <Cong Quy PC@TranCongQuy>
2024-05-29 07:52:02 +0700

    second code

---
+++ LTHDT/src/bai04/vidu2/LopCon.java
@@ -0,0 +1,10 @@
+package bai04.vidu2;
+
+public class LopCon extends LopCha {
+
+	public LopCon(String thuoctinh1, int thuoctinh2, double thuoctinh3) {
+		super(thuoctinh1, thuoctinh2, thuoctinh3);
+		// TODO Auto-generated constructor stub
+	} 
+	
+}
